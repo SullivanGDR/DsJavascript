084929b015303d1921df7955c8e53060c3064b82
SullivanGDR <sullivan04gendre@gmail.com>
2023-10-04 14:29:47 +0200

    Exercice 4

--- js/exo1.js
+++ js/exo1.js
@@ -3,9 +3,19 @@ var listeImages = document.getElementById("liste-image");
 for(var i = 0; i < listeImages.children.length; i++){
     let test = listeImages.children[i];
     test.addEventListener("click", clic, false);
+    test.addEventListener("mouseenter", changeCursor, false);
+    test.addEventListener("mouseleave", changeCursorLeave, false);
 }
 
 function clic(){
     console.log("click");
     console.log(this.children[0].src);
+}
+
+function changeCursor(){
+    this.children[0].style.cursor = "pointer";
+}
+
+function changeCursorLeave(){
+    this.children[0].style.cursor = "default";
 }
