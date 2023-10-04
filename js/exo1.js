46f73c5f8caecf1dd7620ea283b1dcea3306f415
SullivanGDR <sullivan04gendre@gmail.com>
2023-10-04 15:40:13 +0200

    Exercice 4 v2

--- js/exo1.js
+++ js/exo1.js
@@ -11,6 +11,22 @@ function clic(){
     console.log("click");
     console.log(this.children[0].src);
 
+    // Tentative pour un nombre d'images indéfinies.
+    for(var i = 0; i < listeImages.children.length; i++){
+        if (i==0){
+            listeImages.children[0].children[0].src = listeImages.children[listeImages.children.length - 1].children[0].src;
+        }
+        if (i > 0 && i < listeImages.children.length - 1){
+            let lastImage = listeImages.children[i-1].children[0].src;
+            listeImages.children[i].children[0].src = lastImage;
+        }
+        
+        if (i == listeImages.children.length - 1){
+            listeImages.children[i].children[0].src = listeImages.children[i-1].children[0].src;
+        }
+    }
+
+    // Fonctionnel pour les 4 images uniquement.
     let premiereImage = listeImages.children[0].children[0].src;
     let deuxiemeImage = listeImages.children[1].children[0].src;
     let troisiemeImage = listeImages.children[2].children[0].src;
@@ -20,11 +36,6 @@ function clic(){
     listeImages.children[3].children[0].src = troisiemeImage;
     listeImages.children[0].children[0].src = quatriemeImage;
     listeImages.children[1].children[0].src = premiereImage;
-
-    // listeImages.children[0].children[0].src = quatriemeImage;
-    // listeImages.children[1].children[0].src = deuxiemeImage;
-    // listeImages.children[2].children[0].src = troisiemeImage;
-    // listeImages.children[3].children[0].src = premiereImage;
 }
 
 function changeCursor(){
